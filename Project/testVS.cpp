ac442d790dcf6ee605a8899aff37c62e724137d7
A-Piyas-04 <ahnafpias@iut-dhaka.edu>
2024-10-06 18:26:54 +0600

    "Initialized implementing first feature but just as a basic prototype"

---
+++ Project/testVS.cpp
@@ -0,0 +1,220 @@
+#include <iostream>
+#define MAX_MEDICINES 100
+
+using namespace std;
+
+
+void myStrcpy(char* destination, const char* source) {
+    int i = 0;
+    while (source[i] != '\0') {
+        destination[i] = source[i];
+        i++;
+    }
+    destination[i] = '\0'; // Null-terminate the destination string
+}
+
+
+int myStrcmp(const char* str1, const char* str2) {
+    int i = 0;
+    while (str1[i] != '\0' && str2[i] != '\0') {
+        if (str1[i] != str2[i]) {
+            return str1[i] - str2[i];
+        }
+        i++;
+    }
+    return str1[i] - str2[i]; // Compare based on the length
+}
+
+
+int myStrlen(const char* str) {
+    int length = 0;
+    while (str[length] != '\0') {
+        length++;
+    }
+    return length;
+}
+
+
+void padString(char* str, int width) {
+    int len = myStrlen(str);
+    for (int i = len; i < width; i++) {
+        str[i] = ' '; // Pad with spaces
+    }
+    str[width] = '\0'; // Null-terminate the padded s
+}
+
+
+class Medicine {
+private:
+    char name[50];
+    char genericName[50];
+    char supplier[50];
+    float price;
+    int quantity;
+    char expiryDate[15];
+
+public:
+    Medicine() {}
+
+   
+    Medicine(const char* n, const char* gName, const char* supp, float pr, int qty, const char* exp) {
+        myStrcpy(name, n);
+        myStrcpy(genericName, gName);
+        myStrcpy(supplier, supp);
+        price = pr;
+        quantity = qty;
+        myStrcpy(expiryDate, exp);
+    }
+
+   
+    const char* getName() { return name; }
+    const char* getGenericName() { return genericName; }
+    float getPrice() { return price; }
+
+ 
+    void display() {
+        char namePad[50];
+        char genericNamePad[50];
+        char supplierPad[50];
+        padString(name, 15);
+        padString(genericName, 15);
+        padString(supplier, 20);
+        
+        snprintf(namePad, sizeof(namePad), "%s", name);
+        snprintf(genericNamePad, sizeof(genericNamePad), "%s", genericName);
+        snprintf(supplierPad, sizeof(supplierPad), "%s", supplier);
+
+        cout << "| " << namePad 
+             << "| " << genericNamePad 
+             << "| " << supplierPad 
+             << "   | " << price 
+             << "       | " << quantity 
+             << "      | " << expiryDate 
+             << "|" << endl;
+    }
+};
+
+
+class Pharmacy {
+private:
+    Medicine medicines[MAX_MEDICINES];
+    int medicineCount;
+
+public:
+    Pharmacy() {
+        medicineCount = 0;
+    }
+
+ 
+    void loadData(const char* filename) {
+        FILE* file = fopen(filename, "r");
+        if (!file) {
+            cout << "Error opening file!" << endl;
+            return;
+        }
+
+        char name[50], genericName[50], supplier[50], expiryDate[15];
+        float price;
+        int quantity;
+
+        while (fscanf(file, "%s %s %s %f %d %s", name, genericName, supplier, &price, &quantity, expiryDate) != EOF) {
+            medicines[medicineCount++] = Medicine(name, genericName, supplier, price, quantity, expiryDate);
+        }
+        fclose(file);
+    }
+
+ 
+    void printHeader() {
+        cout << "+-----------------+-----------------+----------------------+----------+----------+-------------+" << endl;
+        cout << "| Name            | Generic Name    | Supplier             | Price    | Quantity | Expiry Date |" << endl;
+        cout << "+-----------------+-----------------+----------------------+----------+----------+-------------+" << endl;
+    }
+
+
+    void printFooter() {
+        cout << "+-----------------+-----------------+----------------------+----------+----------+-------------+" << endl;
+    }
+
+ 
+    void searchMedicine(const char* searchTerm) {
+        printHeader();
+        for (int i = 0; i < medicineCount; i++) {
+            if (myStrcmp(medicines[i].getName(), searchTerm) == 0 || myStrcmp(medicines[i].getGenericName(), searchTerm) == 0) {
+                medicines[i].display();
+            }
+        }
+        printFooter();
+    }
+
+
+    void swap(Medicine &a, Medicine &b) {
+        Medicine temp = a;
+        a = b;
+        b = temp;
+    }
+
+
+    void sortMedicines(bool byPrice, bool ascending) {
+        for (int i = 0; i < medicineCount; i++) {
+            for (int j = i + 1; j < medicineCount; j++) {
+                if (byPrice) {
+                    if ((ascending && medicines[i].getPrice() > medicines[j].getPrice()) ||
+                        (!ascending && medicines[i].getPrice() < medicines[j].getPrice())) {
+                        swap(medicines[i], medicines[j]);
+                    }
+                } else {
+                    if ((ascending && myStrcmp(medicines[i].getName(), medicines[j].getName()) > 0) ||
+                        (!ascending && myStrcmp(medicines[i].getName(), medicines[j].getName()) < 0)) {
+                        swap(medicines[i], medicines[j]);
+                    }
+                }
+            }
+        }
+    }
+
+
+    void filterMedicines(float minPrice, float maxPrice) {
+        printHeader();
+        for (int i = 0; i < medicineCount; i++) {
+            if (medicines[i].getPrice() >= minPrice && medicines[i].getPrice() <= maxPrice) {
+                medicines[i].display();
+            }
+        }
+        printFooter();
+    }
+
+
+    void displayAllMedicines() {
+        printHeader();
+        for (int i = 0; i < medicineCount; i++) {
+            medicines[i].display();
+        }
+        printFooter();
+    }
+};
+
+
+int main() {
+    Pharmacy pharmacy;
+
+
+    pharmacy.loadData("medicine_data.txt");
+
+  
+    cout << "\nAll Medicines:\n";
+    pharmacy.displayAllMedicines();
+
+  
+    cout << "\nSearch Results for 'Paracetamol':\n";
+    pharmacy.searchMedicine("Paracetamol");
+
+  
+    cout << "\nMedicines sorted by Price (Ascending):\n";
+    pharmacy.sortMedicines(true, true);
+    pharmacy.displayAllMedicines();
+  
+    cout << "\nMedicines with price between 10 and 50:\n";
+    pharmacy.filterMedicines(10, 50);
+
+    return 0;
+}
