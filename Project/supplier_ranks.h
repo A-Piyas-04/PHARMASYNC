4076538669b83c6fb83dea90406b887eba7df389
Nafiz0081 <sameurrahman99@gmail.com>
2025-03-18 20:43:52 +0600

    Newly genarated output files

--- Project/supplier_ranks.h
+++ Project/supplier_ranks.h
@@ -1,23 +1,27 @@
 #ifndef SUPPLIER_RANKS_H
 #define SUPPLIER_RANKS_H
 
+#include <fstream>
+#include <cstring>
+#include "utility.h"
+
 using namespace std;
 
 class SupplierRanking {
 private:
-    static const int MAX_SUPPLIERS = 50;
+    static const int MAX_SUPPLIERS = 100;
     char suppliers[MAX_SUPPLIERS][50];
     int salesCount[MAX_SUPPLIERS];
     int supplierCount;
 
     void loadRankings();
-    void saveRankings();
-    int findSupplierIndex(const char* supplier);
+    void saveRankings() const;
+    int findSupplier(const char* supplier) const;
 
 public:
     SupplierRanking();
     void updateSupplierSales(const char* supplier, int quantity);
-    void displayRankings();
+    void displayRankings() const;
 };
 
 #endif
